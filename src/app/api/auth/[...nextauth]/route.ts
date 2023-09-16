908d320ee2788334b368cfd1859d07136e3f564b
Tarek Elkanaria <tarekelkanaria@gmail.com>
2023-09-16 20:59:06 +0300

    Modify firebase credential

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,15 +2,19 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
-import { db } from "@/firebase/config";
+import { cert } from "firebase-admin/app";
 import type { NextAuthOptions } from "next-auth";
-import type { FirebaseAdapterConfig } from "@next-auth/firebase-adapter";
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
 const FACEBOOK_CLIENT_ID = process.env.FACEBOOK_CLIENT_ID!;
 const FACEBOOK_CLIENT_SECRET = process.env.FACEBOOK_CLIENT_SECRET!;
 const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET;
+const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
+const FIREBASE_CLIENT_EMAIL = process.env.FIREBASE_CLIENT_EMAIL;
+const FIREBASE_PRIVATE_KEY = JSON.parse(
+  process.env.FIREBASE_PRIVATE_KEY!
+).private_key;
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -23,7 +27,13 @@ export const authOptions: NextAuthOptions = {
       clientSecret: FACEBOOK_CLIENT_SECRET,
     }),
   ],
-  adapter: FirestoreAdapter(db as FirebaseAdapterConfig),
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: FIREBASE_PROJECT_ID,
+      clientEmail: FIREBASE_CLIENT_EMAIL,
+      privateKey: FIREBASE_PRIVATE_KEY,
+    }),
+  }),
   pages: {
     signIn: "/auth/signin",
   },
